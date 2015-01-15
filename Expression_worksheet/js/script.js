898f7510f9b3a061abcb3f2b663cc7d47ec13619
acatalepsic <acatalepsic@gmail.com>
2015-01-15 17:50:35 -0500

    .

--- Expression_worksheet/js/script.js
+++ Expression_worksheet/js/script.js
@@ -56,3 +56,26 @@ var average = groceryCost / 5
 
 //Print out average grocery cost.
 console.log("You have spent a total of $"+ groceryCost +" over 5 weeks. That is an average of $"+ average +" per week.")
+
+
+
+//Calculate the discount price of a desk
+var price = 2080;
+var discountPercent = 45;
+var item = "mahogany desk";
+var tax = 7.5;
+
+//Find the discount amount by dividing the discount percent by 100% and then multiplying the answer by the original price
+var discountAmount = discountPercent / 100 * price;
+
+//Find the discount price by subtracting the discount amount from the original price
+var discount = price - discountAmount;
+
+//Find the tax amount by diving the tax amount by 100 and multiplying it by the discounted price.
+var taxAmount = tax / 100 * discount
+
+//Find the total amount with tax by adding the tax amount to the discounted price.
+var withTax = discount + taxAmount;
+
+//Print out total
+console.log("Your "+ item +" was originally $"+ price +", but after a "+ discountPercent +"% discount, it is now $"+ discount +" without tax, and $"+ withTax +" with tax.");
