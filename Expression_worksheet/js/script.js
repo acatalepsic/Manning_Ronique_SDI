e183cc10055bfa4f0fb873d25000322f058df0c6
acatalepsic <acatalepsic@gmail.com>
2015-01-15 17:48:51 -0500

    .

--- Expression_worksheet/js/script.js
+++ Expression_worksheet/js/script.js
@@ -14,3 +14,20 @@ var dogAge = humanAge * aging;
 
 //Print out Sparky's dog age
 console.log("Sparky is "+ humanAge +" years old which is "+ dogAge +" in dog years.")
+
+
+
+
+//Calculate how much of the pizza party-goers will get at the party
+var pizza = 10;
+var people = 25;
+var slices = 8;
+
+//Find the total number of slices by multiplying the number of slices by the number of pizzas.
+var slicesTotal = pizza * slices;
+
+// Divide total number of slices into the amount of people attending.
+var perPerson = pizza * slices / people;
+
+//Print out the number of slices each person gets
+console.log("Each person ate "+ slicesTotal +" slices of pizza at the party.")
