5d9124efda17d22541dec79cf099a368751bcfd9
acatalepsic <acatalepsic@gmail.com>
2015-01-15 17:38:24 -0500

    Week 2 Examples
    All of the examples from week two’s lessons.

---
+++ Week 2 Examples/Expressions - Age Example/js/script.js
@@ -0,0 +1,7 @@
+//Expressions - Age Example
+
+
+var yearBorn = 1993;
+var currentYear = 2014;
+var age = currentYear - yearBorn;
+console.log(age);
---
+++ Week 2 Examples/Expressions - Concatenation/js/script.js
@@ -0,0 +1,14 @@
+//Expressions - Concatenating Strings
+
+var firstName = "Kermit";
+var lastName = "The Frog";
+var fullname = String(firstName)+" "+ String(lastName);
+
+console.log(fullname)
+
+
+var a = "6";
+var b = "7";
+var result = a + b
+
+console.log(result)
