6acc265df28bb5244855ffd44139307a6745c0a9
acatalepsic <acatalepsic@gmail.com>
2015-01-15 17:47:36 -0500

    Expressions_worksheet
    Files for Week 2’s activity, the Expressions worksheet.

---
+++ Expression_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Ronique Manning
+ SDI Section 03
+ Activity: Expression Worksheet
+ 01-13-2015
+ */
+
+//Calculate Sparky's Age
+var humanAge = 10;
+var aging = 7;
+
+//Multiply human age by dog age
+var dogAge = humanAge * aging;
+
+//Print out Sparky's dog age
+console.log("Sparky is "+ humanAge +" years old which is "+ dogAge +" in dog years.")
